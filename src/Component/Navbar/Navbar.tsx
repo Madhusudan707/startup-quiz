eca23ea366b98f1e49a6ef3b0d9c5d0e9f7672d6
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-21 20:56:18 +0530

    add: menu toggle hook

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -1,15 +1,9 @@
-
-import {useState} from 'react'
+import { useMenuToggle } from '../hooks'
 import './navbar.css'
 
+export const Navbar = () => {
 
-export const Navbar = () => {   
-
-    const [isShowMenu,setIsShowMenu] = useState(true)
-
-    const hamburgerHandler = ()=>{
-            setIsShowMenu(!isShowMenu)
-    }
+    const { menuToggleHandler, isShowMenu } = useMenuToggle()
     return (
         <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-green-400  to-green-700 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
@@ -19,14 +13,14 @@ export const Navbar = () => {
                 <span className="font-semibold text-xl tracking-tight">Startup-Quiz</span>
             </div>
             <div className="block lg:hidden">
-                <button onClick={hamburgerHandler} className="flex items-center px-3 py-2 border rounded text-teal-100 border-teal-400 hover:text-white hover:border-white" id="navbar-btn">
+                <button onClick={menuToggleHandler} className="flex items-center px-3 py-2 border rounded text-teal-100 border-teal-400 hover:text-white hover:border-white" id="navbar-btn">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <title>Menu</title>
                         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                     </svg>
                 </button>
             </div>
-            <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isShowMenu?"navbar-toggle":null}`} id="navbar">
+            <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isShowMenu ? "navbar-toggle" : null}`} id="navbar">
                 <div className="text-sm lg:flex-grow  text-center lg:text-right text-white hover:text-black">
                     <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-100 hover:text-white mr-4 text-lg">
                         Home
