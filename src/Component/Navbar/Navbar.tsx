35de05e3c68c8ecf452816159c20088fc5241dc2
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-22 07:00:57 +0530

    update: navbar

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
                 {/* <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
                     <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
                 </svg> */}
-                <span className="font-semibold text-xl tracking-tight">Startup-Quiz</span>
+                <span className="font-semibold  tracking-tight text-4xl">Startup-Quiz</span>
             </div>
             <div className="block lg:hidden">
                 <button onClick={menuToggleHandler} className="flex items-center px-3 py-2 border rounded text-teal-100 border-teal-400 hover:text-white hover:border-white" id="navbar-btn">
@@ -29,7 +29,7 @@ export const Navbar = () => {
                         About Us
                     </a>
                     <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-100 hover:text-white mr-4 text-lg">
-                        Service
+                        Create Quiz
                     </a>
                     <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-100 hover:text-white text-lg">
                         Contact US
