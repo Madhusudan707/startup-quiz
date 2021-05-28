498209b28ce97b1632ac3b44be7dd3650e22563e
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-28 10:51:34 +0530

    add: index

---
+++ src/Component/index.js
@@ -0,0 +1,7 @@
+export {Home} from './Home/Home'
+export {Navbar} from './Navbar/Navbar'
+export {QuizCard} from './QuizCard/QuizCard'
+export {Rules} from './Modal/Rules'
+export {Close} from './Modal/Close'
+export {Loader} from './Loader/Loader'
+
