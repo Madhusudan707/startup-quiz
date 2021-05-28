bbcd4fa6af0c2082df4080229ffbd1dd96035139
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-28 10:47:16 +0530

    add: rules modal

---
+++ src/Component/Modal/Close.jsx
@@ -0,0 +1,11 @@
+
+
+export const Close = ({nameClass}) => {
+    return (
+        <a href="#close" title="Close">
+        <span className={nameClass}>
+          <i className="fas fa-times"></i>
+        </span>
+      </a>
+    )
+}
