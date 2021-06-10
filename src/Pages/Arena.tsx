47afefe3099c8b34b37d974900be2d733bb747a2
XDEV200 <xdev200200@gmail.com>
2021-06-10 11:19:35 +0530

    fix: login in arena and home

--- src/Pages/Arena.tsx
+++ src/Pages/Arena.tsx
@@ -2,11 +2,22 @@ import { QuizCard, Loader, Rules } from '../Components'
 import { useFetchQuiz } from '../hooks'
 import { useUser } from '../contexts'
 import { useNavigate } from "react-router-dom"
-
+import {useEffect} from 'react'
 export const Arena = () => {
     const { loader, quizInfo, setQuiz } = useFetchQuiz()
-    const { isUserLoggedIn } = useUser()
+    const { isUserLoggedIn,setIsUserLoggedIn } = useUser()
     const navigate = useNavigate()
+    useEffect(()=>{
+        console.log(localStorage.getItem("login"))
+        if(localStorage.getItem("login")){
+            const loginData:any = localStorage.getItem("login")
+            const login:any=JSON.parse(loginData)
+            setIsUserLoggedIn(login.isUserLoggedIn)
+            // if(login.isUserLoggedIn){
+            //     navigate('/arena')
+            // }
+        }
+    },[])
     return (
         <>
             {
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,22 +1,5 @@
-import {useEffect} from 'react'
 import { Heading, Features } from '../Components'
-import {useUser} from '../contexts'
-import {useNavigate} from 'react-router-dom'
-
 export const Home = () => {
-    const { isUserLoggedIn,setIsUserLoggedIn } = useUser()
-    const navigate = useNavigate()
-    useEffect(()=>{
-        if(!isUserLoggedIn){
-            const loginData:any = localStorage.getItem("login")
-            const login:any=JSON.parse(loginData)
-            setIsUserLoggedIn(login.isUserLoggedIn)
-
-            if(login.isUserLoggedIn){
-                navigate('/arena')
-            }
-        }
-    },[])
     return (
         <div className="flex flex-col mt-32 ">
             <>
